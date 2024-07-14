4ac02a3c21c0981fa26ca4a92470d942a3b2f2e6
Chris Adan <55331613+ChrisAdan@users.noreply.github.com>
2024-07-14 14:24:53 -0400

    Implement playRound to check round winner

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const NUMROUNDS = 5;
 const CHOICES = ["Rock", "Paper", "Scissors"];
+let humanScore = 0;
+let computerScore = 0;
+let currentRound = 1;
 
 // Introduction
 console.log("Welcome to Rock Paper Scissors");
@@ -37,6 +40,7 @@ function getHumanChoice() {
   do {
     humanChoice = prompt("Rock, Paper, Scissors?");
   } while (!isValidHumanChoice(humanChoice));
+  return humanChoice.at(0).toUpperCase().concat(humanChoice.slice(1));
 }
 
 function isValidHumanChoice(choice) {
@@ -51,8 +55,62 @@ function isValidHumanChoice(choice) {
     return true;
   }
 }
-// Declare players score variables
-
 // Play a round
+function playRound(humanChoice, computerChoice) {
+  // Announce round number
+  console.log(`Round ${currentRound}`);
+  // Compare human and computer choice
+  // if human and computer choices are same - tie
+  // Strategy: if not tie, check 3 winning cases for an a->b relationship, if no win, check the other way around
+  // log winner in console or announce tie
+  // increment winner's score
+  if (humanChoice === computerChoice) {
+    // Tie
+  } else if (isWinner(humanChoice, computerChoice)) {
+    // Human wins
+    humanScore++;
+    console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+  } else if (isWinner(computerChoice, humanChoice)) {
+    // Computer Wins
+    computerScore++;
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+  } else {
+    throw new TypeError("How did we get here?");
+  }
+  //   Report current scores
+  console.log(
+    `The score is now: Player: ${humanScore} | Computer: ${computerScore}`
+  );
+  // increment round number
+  currentRound++;
+}
+
+function isWinner(a, b) {
+  // Six cases:
+  // If human = rock and computer = scissors - human wins
+  // if human = rock and computer = paper - computer wins
+  // if human = paper and computer = rock - human wins
+  // if human = paper and computer = scissors - computer wins
+  // if human = scissors and computer = paper - human wins
+  // if human = scissors and computer = rock - computer wins
+  // 3 subcases
+  // if a = rock and b = scissors, return a
+  if (a === "Rock" && b === "Scissors") {
+    return true;
+    // if a = paper and b = rock, return a
+  } else if (a === "Paper" && b === "Rock") {
+    return true;
+    // if a = scissors and b = paper, return a
+  } else if (a === "Scissors" && b === "Paper") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Generate Computer choice
+// const computerChoice = getComputerChoice();
+// Get human choice
+// const humanChoice = getHumanChoice();
 
 // Play entire game (5 rounds)

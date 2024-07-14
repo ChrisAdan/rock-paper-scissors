be8ea897ad6fa13ec95e2d3044b85de3a89128f9
Chris Adan <55331613+ChrisAdan@users.noreply.github.com>
2024-07-14 15:28:23 -0400

    Cleanup + refactor comments

--- index.js
+++ index.js
@@ -4,59 +4,39 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRound = 1;
 
-// Get computer choice among Rock, Paper, Scissors
 function getComputerChoice() {
-  // Store possible outcomes in an array;
   let randomSelection = Math.floor(Math.random() * 100) % 3;
-  // Generate a random number with 3 outcomes
   let computerChoice = CHOICES[randomSelection];
-  // Create a string variable and store the value rock/paper/scissors based on random number
-  //   console.log(`Computer chose ${computerChoice}`);
   return computerChoice;
 }
 
-// Get human choice
 function getHumanChoice() {
-  // Ask user to input a choice
-  // Repeat until choice is valid
-  // Validate the choice
-  // Is choice a string?
-  // catch TypeError
-  // Is choice 'rock', 'paper', or 'scissors'?
-  // catch TypeError
-  // If choice is valid, create a new string variable and store the user choice
-  // return the user choice
   let humanChoice;
   do {
     humanChoice = prompt("Rock, Paper, Scissors?");
   } while (humanChoice && !isValidHumanChoice(humanChoice));
+  //   I want to reformat the string to Title case
   return humanChoice.at(0).toUpperCase().concat(humanChoice.slice(1));
 }
 
-function isValidHumanChoice(choice) {
-  if (!choice) {
+function isValidHumanChoice(currentChoice) {
+  if (!currentChoice) {
     console.log("Exiting program");
   } else if (
-    !CHOICES.find((str) => str.toLowerCase() === choice.toLowerCase())
+    !CHOICES.find(
+      (testChoice) => testChoice.toLowerCase() === currentChoice.toLowerCase()
+    )
   ) {
     console.log("Please input a valid choice (rock, paper, or scissors)");
     return false;
   } else {
-    // console.log(`Human chose ${choice}`);
     return true;
   }
 }
-// Play a round
 function playRound(humanChoice, computerChoice) {
-  // Announce round number
   let round = `Round ${currentRound}`;
+  // I think inline grouping looks nice
   console.group(round);
-  //   console.log(`Round ${currentRound}`);
-  // Compare human and computer choice
-  // if human and computer choices are same - tie
-  // Strategy: if not tie, check 3 winning cases for an a->b relationship, if no win, check the other way around
-  // log winner in console or announce tie
-  // increment winner's score
   if (humanChoice === computerChoice) {
     // Tie
     console.log(`It's a tie! You both chose ${humanChoice || computerChoice}`);
@@ -71,31 +51,19 @@ function playRound(humanChoice, computerChoice) {
   } else {
     throw new TypeError("How did we get here?");
   }
-  //   Report current scores
+
   console.log(
     `The score is now: Player: ${humanScore} | Computer: ${computerScore}`
   );
   console.groupEnd(round);
-  // increment round number
   currentRound++;
 }
 
 function isWinner(a, b) {
-  // Six cases:
-  // If human = rock and computer = scissors - human wins
-  // if human = rock and computer = paper - computer wins
-  // if human = paper and computer = rock - human wins
-  // if human = paper and computer = scissors - computer wins
-  // if human = scissors and computer = paper - human wins
-  // if human = scissors and computer = rock - computer wins
-  // 3 subcases
-  // if a = rock and b = scissors, return a
   if (a === "Rock" && b === "Scissors") {
     return true;
-    // if a = paper and b = rock, return a
   } else if (a === "Paper" && b === "Rock") {
     return true;
-    // if a = scissors and b = paper, return a
   } else if (a === "Scissors" && b === "Paper") {
     return true;
   } else {
@@ -123,7 +91,8 @@ function announceWinner() {
     ? console.log(`You win! Great work.`)
     : humanScore < computerScore
     ? console.log(`You lose :( big sad.`)
-    : console.log("Seriously, how did we get here?");
+    : // Should not be reachable
+      console.log("Seriously, how did we get here?");
   console.groupEnd(`Game Summary`);
 }
 

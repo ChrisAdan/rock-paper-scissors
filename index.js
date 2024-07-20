44f208f7a3c9f58812b436e58f77136a56203868
Chris Adan <55331613+ChrisAdan@users.noreply.github.com>
2024-07-20 16:39:20 -0400

    Implement round and score tracking

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const NUMROUNDS = 1;
 const CHOICES = ["Rock", "Paper", "Scissors"];
-let humanScore = 0;
-let computerScore = 0;
-let currentRound = 1;
+let playerScore;
+let computerScore;
+let currentRound;
 
 //  Game logic
 function getComputerChoice() {
@@ -11,55 +11,23 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-function getHumanChoice() {
-  let humanChoice;
-  do {
-    humanChoice = prompt("Rock, Paper, Scissors?");
-  } while (humanChoice && !isValidHumanChoice(humanChoice));
-  //   I want to reformat the string to Title case
-  return humanChoice.at(0).toUpperCase().concat(humanChoice.slice(1));
-}
-
-function isValidHumanChoice(currentChoice) {
-  if (!currentChoice) {
-    console.log("Exiting program");
-  } else if (
-    !CHOICES.find(
-      (testChoice) => testChoice.toLowerCase() === currentChoice.toLowerCase()
-    )
-  ) {
-    console.log("Please input a valid choice (rock, paper, or scissors)");
-    return false;
-  } else {
-    return true;
-  }
-}
-function playRound(humanChoice, computerChoice) {
-  let round = `Round ${currentRound}`;
-  // I think inline grouping looks nice
-  console.group(round);
-  if (humanChoice === computerChoice) {
+function playRound(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
     // Tie
-    console.log(`It's a tie! You both chose ${humanChoice || computerChoice}`);
-  } else if (isWinner(humanChoice, computerChoice)) {
-    // Human wins
-    humanScore++;
-    console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-  } else if (isWinner(computerChoice, humanChoice)) {
+    return false;
+  } else if (isWinner(playerChoice, computerChoice)) {
+    // player wins
+    playerScore += 1;
+    return "player";
+  } else if (isWinner(computerChoice, playerChoice)) {
     // Computer Wins
-    computerScore++;
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+    computerScore += 1;
+    return "ai";
   } else {
     throw new TypeError(
-      `Invalid choice argument: human (${humanChoice}) | ai (${computerChoice})`
+      `Invalid choice argument: player (${playerChoice}) | ai (${computerChoice})`
     );
   }
-
-  console.log(
-    `The score is now: Player: ${humanScore} | Computer: ${computerScore}`
-  );
-  console.groupEnd(round);
-  currentRound++;
 }
 
 function isWinner(a, b) {
@@ -73,65 +41,58 @@ function isWinner(a, b) {
     return false;
   }
 }
-// function playGame() {
-//   while (currentRound <= NUMROUNDS) {
-//     let computerChoice = getComputerChoice();
-//     let humanChoice = getHumanChoice();
-//     playRound(humanChoice, computerChoice);
-//   }
-//   announceWinner();
-// }
 
-function announceWinner() {
-  console.group(`Game Summary`);
-  console.log(
-    `Gamer over! Final Scores: Player: ${humanScore} | Computer: ${computerScore}`
-  );
-  humanScore === computerScore
-    ? console.log(`It's a tie!`)
-    : humanScore > computerScore
-    ? console.log(`You win! Great work.`)
-    : humanScore < computerScore
-    ? console.log(`You lose :( big sad.`)
-    : // Should not be reachable
-      console.log("Seriously, how did we get here?");
-  console.groupEnd(`Game Summary`);
-}
-
-function runProgram() {
-  let continueGame = true;
-  showIntroduction();
-  do {
-    playGame();
-    let selection = prompt("Play again? Y/N");
-    if (!selection || selection.toLowerCase() === "n") {
-      continueGame = false;
-    } else {
-      console.log("Starting new game");
-      currentRound = 1;
-      humanScore = 0;
-      computerScore = 0;
-    }
-  } while (continueGame);
-  console.log("Thanks for playing! Goodbye.");
-}
-
-function showIntroduction() {
-  console.log(
-    `You will play ${NUMROUNDS} rounds. Whoever has the most wins at the end is the winner!`
-  );
-  console.log("May the odds be ever in your favor");
-}
 // DOM Manipulation
 
+document.addEventListener("DOMContentLoaded", setup);
+
+function setup() {
+  playerScore = 0;
+  computerScore = 0;
+  currentRound = 0;
+  updateRoundCounter();
+  updateScore("player");
+  updateScore("ai");
+}
+// Click to start game
+const playButton = document.querySelector("#start-game");
+playButton.addEventListener("click", setup);
+
+// Click card to play round
 const cards = document.querySelectorAll(".weapon");
 cards.forEach((card) => {
-  let playerChoice = card.getAttribute("id");
-  playerChoice = playerChoice
-    .at(0)
-    .toUpperCase()
-    .concat(playerChoice.slice(1).toLowerCase());
   card.addEventListener("click", () => {
-    playRound(playerChoice, getComputerChoice());
+    let computerChoice = getComputerChoice();
+    let playerChoice = card.getAttribute("id");
+    playerChoice = playerChoice
+      .at(0)
+      .toUpperCase()
+      .concat(playerChoice.slice(1).toLowerCase());
+    const roundWinner = playRound(playerChoice, computerChoice);
+    updateRoundCounter();
+    if (roundWinner) {
+      updateScore(roundWinner);
+    }
+    currentRound += 1;
   });
 });
+
+function updateRoundCounter() {
+  const roundCounter = document.querySelector(".round-counter");
+  roundCounter.textContent = currentRound;
+}
+
+function updateScore(winner) {
+  const winnerScoreDisplay = document.querySelector(".".concat(winner));
+  if (winner === "player") {
+    winnerScoreDisplay.textContent = playerScore;
+  } else if (winner === "ai") {
+    winnerScoreDisplay.textContent = computerScore;
+  } else {
+    throw new TypeError(`Invalid input ${winner}`);
+  }
+}
+
+function generateRoundReport(roundWinner) {
+  // You lose! Paper beats rock
+}
